3ec615826b5199b1daed7782111669ae6c9ba9e4
Nina Bozhanova <nbozhanova@gmail.com>
2024-06-02 18:33:50 -0700

    Music Player Application v2

--- js/player.js
+++ js/player.js
@@ -31,21 +31,29 @@ class Album {
     }
 }
 
+const albumArray = ['Operation Ivy - Energy', 'Blink 182 - Dude Ranch', 'New Found Glory - Sticks and Stones']
+let classArray = []
+let select = document.getElementById("album")
+let favorite = document.getElementById("favoriteAlbum")
+let favoriteButton = document.getElementById("favButton")
+let playButton = document.getElementById("play")
+
 var jbox = new Jukebox()
-const album1 = new Album('Operation Ivy', 'Energy')
-const album2 = new Album('Blink 182', 'Dude Ranch')
-const album3 = new Album('New Found Glory', 'Sticks and Stones')
 
-jbox.addAlbum(album1)
-jbox.addAlbum(album2)
-jbox.addAlbum(album3)
+for (let album of albumArray) {
+    let element = document.createElement("option")
+    element.textContent = album
+    select.appendChild(element)
+    let albumClass = new Album(album.split(" - ")[0], album.split(" - ")[1])
+    classArray.push(albumClass)
+    jbox.addAlbum(albumClass)
+}
 
-album1.play()
-album2.play()
-album2.play()
-album2.play()
-album2.play()
-album2.play()
-album3.play()
+playButton.addEventListener('click', () => {
+    let index = albumArray.indexOf(select.value)
+    classArray[index].play()
+})
 
-console.log(`Your favorite album is: ${jbox.favoriteAlbum()}`)
+favoriteButton.addEventListener('click', () => {
+    favorite.innerHTML = `Your favorite album is: ${jbox.favoriteAlbum()}`
+})
